ff3a913c4c7ad6194d6bc26802701d0bc763e0ee
rijkvp <rijk@rijkvp.nl>
2023-11-26 12:05:24 +0100

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            fontconfig
+            openssl
+            libxkbcommon
+            libGL
+            vulkan-loader
+            wayland
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          nativeBuildInputs = with pkgs; [ openjdk17-bootstrap gradle ];
+        };
+      }
+    );
+}
+
